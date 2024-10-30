556be41ebcb7bd66b704c57084138eabbffa2901
Halil OZTEKIN <hoztekin81@hotmail.com>
2024-10-30 12:26:45 +0300

    Add project files.

---
+++ UI/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.ServiceModel;
+using TCDogrula;
+using UI.Models;
+
+namespace UI.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			
+			return View();
+		}
+
+		[HttpPost]
+		public async Task <IActionResult> Index (TCViewModel model) 
+		{
+
+			if (ModelState.IsValid)
+			{
+				var binding = new BasicHttpBinding();
+				binding.Security.Mode = BasicHttpSecurityMode.Transport; // HTTPS için
+				binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.None;
+
+
+				// NVI'nin SOAP servisi için endpoint adresi
+				var endpointAddress = new EndpointAddress("https://tckimlik.nvi.gov.tr/Service/KPSPublic.asmx");
+
+				// Client oluþturma
+				using (var client = new TCDogrula.KPSPublicSoapClient(binding, endpointAddress))
+				{
+					try
+					{
+						var response = await client.TCKimlikNoDogrulaAsync(model.CitizenId, model.Name, model.LastName, model.BirthDate);
+
+						ViewBag.SonucBasarili = response.Body.TCKimlikNoDogrulaResult;
+					}
+					catch (Exception ex)
+					{
+						ViewBag.Hata = ex.Message;
+					}
+				}
+
+			}
+			else
+			{
+				return View(model);	
+			}
+
+			
+			return View();
+		}
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ UI/Models/TCViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UI.Models
+{
+	public class TCViewModel
+	{
+		[Required(ErrorMessage = "Ad Alanı zorunludur!")]
+		[Display(Name = "Adınız *")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Soyad Alanı zorunludur!")]
+		[Display(Name = "Soyadınız *")]
+		public string LastName { get; set; }
+
+		[Required(ErrorMessage = "Doğum Tarihi Alanı zorunludur!")]
+		[Display(Name = "Doğum yılınız *")]
+		[Range(1900, 9999, ErrorMessage = "Doğum yılı 4 haneli olmalıdır!")]
+		[RegularExpression(@"^[0-9]*$", ErrorMessage = "Doğum yılı sadece sayı içermelidir!")]
+		public int BirthDate { get; set; }
+
+
+		[Required(ErrorMessage = "T.C. Kimlik Numarası Alanı zorunludur!")]
+		[Display(Name = "T.C. Kimlik Numarası *")]
+		[RegularExpression(@"^[0-9]*$", ErrorMessage = "T.C. Kimlik Numarası alanı sadece sayı içermelidir!")]
+		[Range(10000000000, 99999999999, ErrorMessage = "T.C. Kimlik Numarası 11 haneli olmalıdır!")]
+		public long CitizenId { get; set; }
+    }
+}
